bf2e971512f66596dd6147cfd3b2ed2504f49abd
Mariya <wilinwe@mail.ru>
2021-06-12 03:20:25 +0400

    Lesson_variables_done

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,28 @@
 let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 40+8+23-10;
-console.log(40+8+23-10);
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+let first = "jonas";
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+
+let person = 'Jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
