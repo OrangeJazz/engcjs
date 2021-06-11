52128555dd4e94d494a87b0f4d7bf37f1ac599a4
Mariya <wilinwe@mail.ru>
2021-06-12 03:52:59 +0400

    Lesson_datatype_done

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assig.js
@@ -0,0 +1,16 @@
+let country = 'Russia';
+let continent = 'Europe';
+let population = 147;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
