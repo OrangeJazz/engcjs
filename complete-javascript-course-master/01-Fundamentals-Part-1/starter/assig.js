9a47ac6cb80bda421fc7ae28f0f4b0c0af7e1691
Mariya <wilinwe@mail.ru>
2021-06-12 05:34:57 +0400

    Lesson_BasicOperators_done

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assig.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assig.js
@@ -1,5 +1,5 @@
-let country = 'Russia';
-let continent = 'Europe';
+const country = 'Russia';
+const continent = 'Europe';
 let population = 147;
 
 console.log(country);
@@ -7,10 +7,27 @@ console.log(continent);
 console.log(population);
 
 
-let isIsland = false;
+const isIsland = false;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+language = 'russian';
+//isIsland = true;
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description = country + 
+' is in ' + 
+continent + 
+', and its ' + 
+population + 
+' million people speak ' + 
+language;
+console.log(description);
