e2c4ff52702d192027bf713857360c1be4bccbb1
Mariya <wilinwe@mail.ru>
2021-06-14 05:52:10 +0400

    Challange_2_arrays_done

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenges.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenges.js
@@ -1,3 +1,5 @@
+'use strict';
+/*
 //Challenge 1
 const calcAverage = (num1, num2, num3) => (num1 + num2 + num3) / 3;
 
@@ -22,5 +24,20 @@ avgScoreDolphins = calcAverage(85, 54, 41);
 avgScoreKoalas = calcAverage(23, 34, 27);
 console.log(avgScoreDolphins, avgScoreKoalas);
 checkWinner(avgScoreDolphins, avgScoreKoalas);
+*/
+//Challenge 2
 
+const calcTip = function(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+}
 
+const bills = [125, 555, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[1] + tips[1]];
+console.log(total);
