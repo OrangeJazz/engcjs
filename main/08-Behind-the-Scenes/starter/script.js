e5675b31697c66e8cf53ea3a1f392829afbd2810
Mariya <wilinwe@mail.ru>
2021-06-24 00:29:46 +0400

    js_theory_in_process

--- main/08-Behind-the-Scenes/starter/script.js
+++ main/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,123 @@
 'use strict';
+/*
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'NEW OUTPUT!'
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+  console.log(firstName);
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge();
+
+
+
+//Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//Functions
+
+console.log(addDecl(3, 5));
+// console.log(addExpr(3, 6)); 
+console.log(addArrow);
+// console.log(addArrow(4, 6));
+
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+
+//Exaple
+
+if (!numProducts) deleteShoppingCard();
+
+
+var numProducts = 10;
+
+function deleteShoppingCard() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+
+*/
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+}
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+}
+
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  }
+}
+
+jonas.calcAge();
+
+const matilda = {
+  year:2017,
+}
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+
+// f();
